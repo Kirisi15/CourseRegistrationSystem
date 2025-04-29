841b11781ff252418ec0f1b64941f6ba2d91d093
Kirisi15 <kkirisigan@gmail.com>
2025-04-29 15:24:17 +0530

    Created Post Mapping

--- src/main/java/com/example/Course/Registration/System/Controller/CourseController.java
+++ src/main/java/com/example/Course/Registration/System/Controller/CourseController.java
@@ -5,6 +5,8 @@ import com.example.Course.Registration.System.Model.CourseRegistry;
 import com.example.Course.Registration.System.Service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,6 +26,14 @@ public class CourseController {
     @GetMapping("Courses/enrolled")
     public List<CourseRegistry> enrolledStudents(){
         return courseService.enrolledStudents();
+    }
+
+    @PostMapping("/Courses/register")
+    public String enrollCourse(@RequestParam("name") String name,
+                               @RequestParam("emailId") String emailId,
+                               @RequestParam("courseName") String courseName) {
+        courseService.enrollCourse(name,emailId,courseName);
+                return "Successfully enrolled in the course: " + courseName;
     }
 
 }
